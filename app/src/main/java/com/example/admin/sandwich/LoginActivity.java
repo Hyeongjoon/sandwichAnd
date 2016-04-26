ede58e4c440160fe7ead2c60395a0291e8c1cbc6
Hyeong joon Jang <Hyeong joon Jang>
2016-04-26 23:24:51 +0900

    first!!! 16-04-26

---
+++ app/src/main/java/com/example/admin/sandwich/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.admin.sandwich;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by admin on 2016-04-26.
+ */
+
+public class LoginActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
