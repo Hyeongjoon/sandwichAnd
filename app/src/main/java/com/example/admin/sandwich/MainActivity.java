7d57a223e9f83ee6e9811de51c4687df7d9b9a36
Hyeong joon Jang <Hyeong joon Jang>
2016-05-05 11:54:51 +0900

    16-05-05

--- app/src/main/java/com/example/admin/sandwich/MainActivity.java
+++ app/src/main/java/com/example/admin/sandwich/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.admin.sandwich;
 
 import android.app.ProgressDialog;
+
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+
+import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,31 +26,17 @@ import org.json.JSONObject;
 
 import java.net.URISyntaxException;
 
+
+
 public class MainActivity extends AppCompatActivity {
     ProgressDialog pDialog;
-
-    private AlertDialog mDialog;
-
-    private AlertDialog createDialog() {
-        AlertDialog.Builder ab = new AlertDialog.Builder(this);
-        ab.setTitle("Title");
-        ab.setMessage("my message");
-        ab.setCancelable(true);
-        ab.setNeutralButton(android.R.string.ok,
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
-                    }
-                });
-
-        return ab.create();
-    }
-
+    Handler mHandler = null;
+    AlertDialog dialog;
 
     private Socket mSocket;
     {
         try {
-            mSocket = IO.socket("http://192.168.0.25:3001");
+            mSocket = IO.socket("http://192.168.0.43:3001");
         } catch (URISyntaxException e) {
             System.out.println(e);
         }
@@ -59,10 +48,8 @@ public class MainActivity extends AppCompatActivity {
     private GoogleApiClient client;
 
     private Emitter.Listener resultLogin = new Emitter.Listener() {
-
         @Override
         public void call(Object... args) {
-
 
             JSONObject data = (JSONObject) args[0];
             String result;
@@ -76,10 +63,40 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println("true");
                 pDialog.cancel();
             } else{
-                pDialog.cancel();
+                Thread t = new Thread(new Runnable(){
+                    @Override
+                    public void run() {
+                        mHandler.post(new Runnable(){
+                            @Override
+                            public void run() {
+                                // 알림창 띄우기
+                                pDialog.cancel();
+                                dialog.show();
+                            }
+                        });
+                    }
+                });
+                t.start();
             }
         }
     };
+
+    private void createDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);     // 여기서 this는 Activity의 this
+        // 여기서 부터는 알림창의 속성 설정
+        builder.setTitle("로그인 오류")        // 제목 설정
+                .setMessage("아이디나 비밀번호가 잘못됐습니다.")        // 메세지 설정
+                .setCancelable(true)        // 뒤로 버튼 클릭시 취소 가능 설정
+                .setPositiveButton("확인", new DialogInterface.OnClickListener(){
+                    // 확인 버튼 클릭시 설정
+                    public void onClick(DialogInterface dialog, int whichButton){
+                        dialog.cancel();
+                    }
+                });
+        mHandler = new Handler();
+        dialog = builder.create();    // 알림창 객체 생성
+        // 알림창 띄우기
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,8 +104,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
+        createDialog();
         mSocket.on("resultLogin" , resultLogin);
-        mDialog = createDialog();
         mSocket.connect();
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
@@ -101,8 +118,6 @@ public class MainActivity extends AppCompatActivity {
         String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
         if(!email.matches(emailPattern)){
             Toast.makeText(getApplicationContext() , "invalid email" ,Toast.LENGTH_SHORT).show();
-            //Intent intent = new Intent(this , SocketIOActivity.class);
-            //startActivity(intent);
         } else if(password.length()==0){
             Toast.makeText(getApplicationContext() , "password is empty" ,Toast.LENGTH_SHORT).show();
         } else{
@@ -116,12 +131,12 @@ public class MainActivity extends AppCompatActivity {
             mSocket.emit("loginChk", inform);
             pDialog = ProgressDialog.show(this, "Login.....", "Please wait", true, true);
         }
-        return;
     }
 
     public void No(View view){
-        Intent intent = new Intent(this , NoActivity.class);
+        Intent intent = new Intent(getApplicationContext() , SignUpActivity.class);
         startActivity(intent);
+        finish();
         return;
     }
 
--- app/src/main/java/com/example/admin/sandwich/SignUpActivity.java
+++ app/src/main/java/com/example/admin/sandwich/SignUpActivity.java
@@ -1,16 +1,25 @@
 package com.example.admin.sandwich;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-/**
+/*
  * Created by admin on 2016-04-26.
  */
-public class NoActivity extends Activity {
+public class SignUpActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_no);
+        setContentView(R.layout.activity_sign_up);
     }
+
+    public void temp(){
+
+        return;
+    }
+
+
 }
